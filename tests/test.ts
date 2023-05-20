7561d0cc611f155be43a41f4b05fda6c91058beb
Oleg Ivinskiy <gskitlz@gmail.com>
2023-05-21 00:29:50 +0800

    Add E2E tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,30 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+const loadMoreButton = 'button[aria-label="Load more coffee"]';
+const loadMoreButtonActive = 'button[aria-label="Load more coffee"]:not(:disabled)';
+
+test('header has correct title', async ({ page }) => {
+  await page.goto('/');
+  expect(await page.textContent('header .title')).toBe('Coffee beans');
+});
+
+test('page has Load more button', async ({ page }) => {
+  await page.goto('/');
+  const button = page.locator(loadMoreButton);
+  await button.waitFor();
+});
+
+test('one CoffeBean element after the page load', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForTimeout(50);
+  expect(await page.locator('.coffee-list > li').count()).toBe(1);
+});
+
+test('two CoffeBean elements after clicking Load more button', async ({ page }) => {
+  await page.goto('/');
+  const button = page.locator(loadMoreButtonActive);
+  await button.waitFor();
+  button.click();
+  await page.waitForTimeout(50);
+  expect(await page.locator('.coffee-list > li').count()).toBe(2);
 });
